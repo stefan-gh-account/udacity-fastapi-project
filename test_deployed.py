d432cd4ecda3a0c87cf4d9766445913beda970b5
stefan-gh-account <sberger.jena@googlemail.com>
2025-12-02 11:31:46 +0100

    add runtime.txt for render

---
+++ test_deployed.py
@@ -0,0 +1,19 @@
+import requests
+
+url = "https://udacity-fastapi-project.onrender.com/infer"
+input_dict = {"age":39,
+              "workclass":"Private",
+              "fnlgt":9999,
+              "education":"Bachelors",
+              "marital-status":"Married-civ-spouse",
+              "occupation":"Exec-managerial",
+              "relationship":"Husband",
+              "race":"White",
+              "sex":"Male",
+              "capital-gain":0,
+              "capital-loss":0,
+              "hours-per-week":40,
+              "native-country":"United-States"}
+
+response = requests.post(url, json=input_dict)
+print(response.json())
